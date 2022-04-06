fd36c9aab52a7bf323a10d43d49836cee1ad2546
greatlaki <greatlaki911@gmail.com>
2022-04-06 22:51:09 +0300

    Working with urls in templates

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
-    path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('<slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
