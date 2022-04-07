5baee7119ec18cb2758067b975d9dd7de7841383
greatlaki <greatlaki911@gmail.com>
2022-04-07 12:59:29 +0300

    Form and view of feed back were created

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from django.contrib.auth import login, authenticate
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.mail import send_mail, BadHeaderError
 
 from .models import *
 from .forms import *
@@ -65,5 +66,30 @@ class SignInView(View):
                 login(request, user)
                 return HttpResponseRedirect('/')
         return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
+
+
+class FeedBackView(View):
+    def get(self, request, *args, **kwargs):
+        form = FeedBackForm()
+        return render(request, 'myblog/contact.html', context={
+            'form': form,
+            'title': 'Написать мне'
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(f'От {name} | {subject}', message, from_email, ['amromashov@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Невалидный заголовок')
+            return HttpResponseRedirect('success')
+        return render(request, 'myblog/contact.html', context={
             'form': form,
         })
