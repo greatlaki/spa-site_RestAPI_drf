e9abbac18117cbdf7faa7f4fcac7193676edc15f
greatlaki <greatlaki911@gmail.com>
2022-04-11 23:18:49 +0300

    Api for the side menu

--- drf_blog/api/core/urls.py
+++ drf_blog/api/core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("tags/", TagView.as_view()),
     path("tags/<slug:tag_slug>/", TagDetailView.as_view()),
+    path("aside/", AsideView.as_view()),
 ]
