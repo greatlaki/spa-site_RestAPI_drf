9650fc16aed0520bdd11d53139ba07368b5abc06
greatlaki <greatlaki911@gmail.com>
2022-04-11 22:47:13 +0300

    I made permissions(AllowAny)

--- drf_blog/api/core/views.py
+++ drf_blog/api/core/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .serializers import PostSerializer
 from rest_framework.response import Response
+from rest_framework import permissions
 
 from .models import *
 
@@ -9,3 +10,4 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
