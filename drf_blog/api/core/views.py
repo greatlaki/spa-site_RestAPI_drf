43cdacc6186d22817675fa51eb44a9330d204409
greatlaki <greatlaki911@gmail.com>
2022-04-11 15:57:12 +0300

    Created PostViewSet

--- drf_blog/api/core/views.py
+++ drf_blog/api/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import *
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
