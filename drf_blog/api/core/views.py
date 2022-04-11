617101832306dca85d02ca94ba26a054840a7ce1
greatlaki <greatlaki911@gmail.com>
2022-04-11 22:50:04 +0300

    I made pagination

--- drf_blog/api/core/views.py
+++ drf_blog/api/core/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
 from .serializers import PostSerializer
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import permissions, pagination
 
 from .models import *
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -11,3 +17,4 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
